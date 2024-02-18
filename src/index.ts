c69eb684395a0091f76ccbb91ea004d97d102dc5
Maggie Liu <maggie.j.liu@gmail.com>
2024-02-18 01:37:56 -0800

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,84 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+interface Env {
+	AUTH_TOKEN: string;
+	DKIM_PRIVATE_KEY: string;
 }
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: Env): Promise<Response> {
+		if (!request.headers.has('Authorization')) {
+			return new Response('Unauthorized', {
+				status: 401,
+			});
+		}
+		const authorization = request.headers.get('Authorization') as string;
+		const [scheme, token] = authorization.split(' ');
+		if (!token || scheme !== 'Bearer' || token !== env.AUTH_TOKEN) {
+			return new Response('Unauthorized', {
+				status: 401,
+			});
+		}
+
+		if (!request.headers.get('Content-Type')?.includes('application/json')) {
+			return new Response('Invalid Content-Type', {
+				status: 415,
+			});
+		}
+
+		const body: {
+			to: string | string[];
+			htmlMessage: string;
+			textMessage: string;
+			subject: string;
+			from: string;
+		} = await request.json();
+
+		if (!body.to || !body.htmlMessage || !body.textMessage || !body.subject || !body.from) {
+			return new Response("Doesn't include all required arguments", {
+				status: 400,
+			});
+		}
+
+		let { to, subject, htmlMessage, textMessage, from } = body;
+
+		const res = await fetch('https://api.mailchannels.net/tx/v1/send', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({
+				personalizations: [
+					{
+						to: [
+							{
+								email: to,
+							},
+						],
+						dkim_domain: 'maggieliu.dev',
+						dkim_selector: 'mailchannels',
+						dkim_private_key: env.DKIM_PRIVATE_KEY,
+					},
+				],
+				from: {
+					email: from,
+				},
+				subject,
+				content: [
+					{
+						type: 'text/plain',
+						value: textMessage,
+					},
+					{
+						type: 'text/html',
+						value: htmlMessage,
+					},
+				],
+			}),
+		});
+		console.log(res);
+
+		const message = await res.text();
+		console.log(res.status);
+		console.log(message);
+		return new Response('Hello, world!');
 	},
 };
